38ee607bcb25082dba190ac37bc8cf05c46d614b
王静 <jing.wang@chiefclouds.com>
2023-02-23 16:02:03 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   ];
 
   return (
-    <div className="App">
+    <div className="app_container">
       <Switch>
         {list.map((r) => (
           <MyRoute key={r?.path} {...r} />
--- src/pages/P5/index.jsx
+++ src/pages/P5/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import styles from './index.module.less';
+import classnames from 'classnames';
 
 const P5 = () => {
-  return <>asda</>;
+  return <div className={classnames('main', styles.P5)}>12312</div>;
 };
 export default P5;
